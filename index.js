0ad757e95cd6443b4f91bd0c74834a6030211e69
noncooperatehk <53223012+noncooperatehk@users.noreply.github.com>
2019-08-02 23:52:47 +0800

    implement markdown parser and update indexjs file

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 import "core-js/stable"; //must include this, otherwise Babel does not work.
 import "regenerator-runtime/runtime"; //must include this, otherwise Babel does not work.
-import vfile from "to-vfile";
-import fs from 'fs';
-import unified from "unified";
-import markdown from 'remark-parse'
+import vfile from 'to-vfile'
 import frontMatterParser from './frontMatterParser'
+import markDownParser from "./markDownParser";
 
-// const processor = unified().use(markdown)
-// const data = JSON.stringify(processor.parse(vfile.readSync("./example.md")));
-//
-// fs.writeFile('output.json', data, {encoding: "utf-8"}, (err) => {
-//     if (err) throw err;
-//     console.log('The file has been saved!');
-// });
+async function splitContent(contentStr) {
+    let reg = /(---[\s\S]*---[\s]*)([\s\S]*)/
+    return vfile.readSync("VitaGreen.md").toString().match(reg).slice(1, 3); //get the second and the third element
+}
 
-frontMatterParser.parseFrontMatter("VitaGreen.md").then(e => console.log(e)).catch(e => console.log(e.stack));
+async function main() {
+    let content = await vfile.readSync("VitaGreen.md").toString();
+    let [frontMatter, mdContent] = await splitContent(content);
+    let frontMatterJson = await frontMatterParser.parseFrontMatterString(frontMatter)
+    let markDownASTJson = await markDownParser.mdStringToAST(mdContent);
+}
