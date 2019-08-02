219bf7258cd850fb298534a23d745213eb0be1b6
noncooperatehk <53223012+noncooperatehk@users.noreply.github.com>
2019-08-02 23:57:15 +0800

    add mannual testing code

---
+++ test/test.js
@@ -0,0 +1,13 @@
+import "core-js/stable"; //must include this, otherwise Babel does not work.
+import "regenerator-runtime/runtime"; //must include this, otherwise Babel does not work.
+import frontMatterParser from "../frontMatterParser";
+//test code
+frontMatterParser.parseMdFileFrontMatter("VitaGreen.md").then(e => console.log(e)).catch(e => console.log(e.stack));
+
+let testContent = "---\n test:1\n --- mdContent"
+frontMatterParser.parseFrontMatterString(testContent).then(e => console.log(e)).catch(e => console.log(e.stack));
+
+// fs.writeFile('output.json', data, {encoding: "utf-8"}, (err) => {
+//     if (err) throw err;
+//     console.log('The file has been saved!');
+// });
