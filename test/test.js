de84d2c893143a22ad25b6661379ca7f9f90e3f1
noncooperatehk <53223012+noncooperatehk@users.noreply.github.com>
2019-08-03 00:00:38 +0800

    move test input into test folder

--- test/test.js
+++ test/test.js
@@ -2,7 +2,7 @@ import "core-js/stable"; //must include this, otherwise Babel does not work.
 import "regenerator-runtime/runtime"; //must include this, otherwise Babel does not work.
 import frontMatterParser from "../frontMatterParser";
 //test code
-frontMatterParser.parseMdFileFrontMatter("VitaGreen.md").then(e => console.log(e)).catch(e => console.log(e.stack));
+frontMatterParser.parseMdFileFrontMatter(__dirname + "/VitaGreen.md").then(e => console.log(e)).catch(e => console.log(e.stack));
 
 let testContent = "---\n test:1\n --- mdContent"
 frontMatterParser.parseFrontMatterString(testContent).then(e => console.log(e)).catch(e => console.log(e.stack));
